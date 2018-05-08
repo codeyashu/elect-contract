0746ee4eb68d1dd5692629442fcc262cff5ca464
yashas <yashasycsc@outlook.com>
2018-05-08 22:19:08 +0530

    updated elect contract

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile/compileStorage');
+const { interface, bytecode } = require('./compile/compileElect');
 
 // Provider requires metamask mnemonic and a node in the network.
 // Here infura is used. Sign up at infura.io
--- test/Elect.test.js
+++ test/Elect.test.js
@@ -1,4 +1,39 @@
-// To test Elect contract using mocha
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile/compileElect');
+
+let elect;
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    elect = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode
+        })
+        .send({
+            from: accounts[0], gas: '1000000'
+        })
+})
+
+describe('Elect-- Smart contract', () => {
+
+    it('deploys the contract', () => {
+        assert.ok(elect.options.address);
+    })
+
+    it('checks for manager', async () => {
+        manager = await elect.methods.manager().call({
+            from: accounts[0]
+        })
+        assert(accounts[0], manager);
+    })
+})
+
+
+/* // To test Elect contract using mocha
 
 const assert = require('assert');
 const ganache = require('ganache-cli');
@@ -65,4 +100,4 @@ describe('Elect-- Smart contract', () => {
         })
         assert.equal(5, voters.length);
     })
-})
+}) */
